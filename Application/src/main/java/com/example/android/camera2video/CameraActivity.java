a591914fdb6e06e3d1a63a8e07c15e76e3e2addd
Thomas Taschauer <tomtasche@gmail.com>
2017-04-25 19:42:44 +0200

    turn on display when sensors change

--- Application/src/main/java/com/example/android/camera2video/CameraActivity.java
+++ Application/src/main/java/com/example/android/camera2video/CameraActivity.java
@@ -17,9 +17,24 @@
 package com.example.android.camera2video;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.PowerManager;
 
-public class CameraActivity extends Activity {
+public class CameraActivity extends Activity implements SensorEventListener {
+
+    private PowerManager powerManager;
+    private PowerManager.WakeLock temporaryWakeLock;
+
+    private SensorManager sensorManager;
+    private Sensor proximitySensor;
+    private Sensor lightSensor;
+
+    private int lastLightValue = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +45,71 @@ public class CameraActivity extends Activity {
                     .replace(R.id.container, Camera2VideoFragment.newInstance())
                     .commit();
         }
+
+        powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                "doorguard");
+        wakeLock.acquire();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (proximitySensor == event.sensor) {
+            if (event.values[0] < 5) {
+                wake();
+            } else {
+                unwake();
+            }
+        } else if (lightSensor == event.sensor) {
+            int newValue = (int) event.values[0];
+
+            int valueChange = newValue - lastLightValue;
+            if (lastLightValue > 0 && valueChange > 3) {
+                wake();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        unwake();
+                    }
+                });
+            }
+
+            lastLightValue = newValue;
+        }
+    }
+
+    private void wake() {
+        if (temporaryWakeLock != null && temporaryWakeLock.isHeld()) {
+            return;
+        }
+
+        temporaryWakeLock = powerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.ON_AFTER_RELEASE,
+                "temp-doorguard");
+        temporaryWakeLock.acquire();
+    }
+
+    private void unwake() {
+        if (temporaryWakeLock == null || !temporaryWakeLock.isHeld()) {
+            return;
+        }
+
+        try {
+            temporaryWakeLock.release();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 }
